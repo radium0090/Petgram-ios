49b61f8ebd2793908b3423d67b9d634fae418d41
shi.rei <shi.rei@vega-c.com>
2019-04-08 13:22:40 +0900

    Initial Commit

---
+++ Petgram/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Petgram
+//
+//  Created by 是レイ on 2019/04/08.
+//  Copyright © 2019年 SHI LEI. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Petgram/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Petgram
+//
+//  Created by 是レイ on 2019/04/08.
+//  Copyright © 2019年 SHI LEI. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
